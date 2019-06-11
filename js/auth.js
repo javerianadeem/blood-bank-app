b8ebc90ce04fd7c08e342e67a2f20ffcf0652f99
Javeria <javerianadeem03@gmail.com>
2019-06-11 18:35:04 +0500

    authentication bug resolved

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,3 @@
-var loginDiv = document.getElementById('login-div');
-var welcomeDiv = document.getElementById('welcome-div');
-
 //signup
 const signupForm = document.querySelector('#signup-form')
 signupForm.addEventListener('submit', (e) => {
@@ -15,13 +12,14 @@ signupForm.addEventListener('submit', (e) => {
     });
 });
 
-// //logout
-const logout = document.querySelector('logout');
-logout.addEventListener('click', (e) => {
-    auth.signOut().then(() => {
-console.log('hello')
-    });
-});
+// // //logout
+// const logout = document.querySelector('.logout');
+// logout.addEventListener('click', (e) => {
+//     auth.signOut().then(function() {
+//         window.location = "signup.html"
+// console.log('hello')
+//     });
+// });
 // login
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
@@ -30,6 +28,14 @@ loginForm.addEventListener('submit', (e) => {
     const password = loginForm['login-password'].value;
     auth.signInWithEmailAndPassword(email, password).then(cred => {
         loginForm.reset();
-        alert('helo')
     });
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        window.location = "donote.html"
+        console.log("User Signed In")
+    }
+    
 });
